f86204e07117f47cd07ebb740cb7af14876897b6
cchrrisppatrickk <cchilonsegura@gmail.com>
2026-03-21 15:50:39 -0500

    feat(backend): implementación de JWT, Passport y endpoint de login (Fase 1.5.2)

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @Post('login')
+    async login(@Body() loginDto: LoginDto) {
+        const user = await this.authService.validateUser(
+            loginDto.email,
+            loginDto.password,
+        );
+        return this.authService.login(user);
+    }
+}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+    ) { }
+
+    async validateUser(email: string, pass: string): Promise<any> {
+        const user = await this.usersService.findByEmail(email);
+        if (user && user.password) {
+            const isMatch = await bcrypt.compare(pass, user.password);
+            if (isMatch) {
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                const { password, ...result } = user;
+                return result;
+            }
+        }
+        throw new UnauthorizedException('Credenciales incorrectas');
+    }
+
+    async login(user: any) {
+        const payload = { sub: user.id, roleId: user.roleId };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
+    }
+}
